7537c545bd38d5ac0a8f5b16184a5b35c5e5c27f
Gio Bermudez <rommelgiobermudez@gmail.com>
2026-04-13 05:51:39 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import SearchBar from "./components/SearchBar";
 import Filters from "./components/Filters";
+import ProgressBar from "./ProgressBar";
 
 
 
