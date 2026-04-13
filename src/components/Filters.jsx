d9a6c88cc9d0280ce2157b6b5891a697726e9639
Uriyann <joshuabarotea06@gmail.com>
2026-04-14 06:44:19 +0800

    fileter

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "../styles/Filters.css";
 
 function Filters() {
-  <div className="filter-group">
-    <button className="all-task">ALL TASK</button>
-    <button className="completed">COMPLETED</button>
-    <button className="overdue">OVERDUE</button>
-  </div>;
+  return (
+    <>
+      <div className="filter-group">
+        <button className="all-task">ALL TASK</button>
+        <button className="completed">COMPLETED</button>
+        <button className="overdue">OVERDUE</button>
+      </div>
+    </>
+  );
 }
 
 export default Filters;
