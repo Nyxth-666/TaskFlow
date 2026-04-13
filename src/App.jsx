a4512e236f907b0cd808482120f6f498034abc9f
Uriyann <joshuabarotea06@gmail.com>
2026-04-14 06:10:07 +0800

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
 import TaskForm from "./components/TaskForm";
+import ProgressBar from "./components/ProgressBar";
 
 function App() {
   return (
     <>
       <Header />
       <TaskForm />
+      <div className="progress-boxes">
+        <ProgressBar />
+      </div>
     </>
   );
 }
