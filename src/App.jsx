3f4c64bd8214a5693d0b0603102121c1879f8ac2
Uriyann <joshuabarotea06@gmail.com>
2026-04-13 22:14:03 +0800

    moved

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
+import TaskForm from "./components/TaskForm";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <TaskForm />
+    </>
   );
 }
 
