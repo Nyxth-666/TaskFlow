e483d4ab6bed6daea4408a2534595dde3b3e2912
Uriyann <joshuabarotea06@gmail.com>
2026-04-13 22:57:14 +0800

    NEW

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,34 +1,11 @@
 import "./Filters.css";
 
 function Filters() {
-
-  const handleAllTasks = () => {
-    // logic for all tasks
-  };
-
-  const handleCompleted = () => {
-    // logic for completed tasks
-  };
-
-  const handleOverdue = () => {
-    // logic for overdue tasks
-  };
-
-  return (
-    <div className="task-tabs">
-      <button className="tab-btn" onClick={handleAllTasks}>
-        ALL TASKS
-      </button>
-
-      <button className="tab-btn" onClick={handleCompleted}>
-        COMPLETED
-      </button>
-
-      <button className="tab-btn" onClick={handleOverdue}>
-        OVERDUE
-      </button>
-    </div>
-  );
+  <div className="filter-group">
+    <button className="all-task">ALL TASK</button>
+    <button className="completed">COMPLETED</button>
+    <button className="overdue">OVERDUE</button>
+  </div>;
 }
 
 export default Filters;
