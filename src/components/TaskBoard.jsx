f8762a0419599d63bbb2e5a10d17ba4f56157292
Uriyann <joshuabarotea06@gmail.com>
2026-04-13 22:05:15 +0800

    added new taskboard

---
+++ src/components/TaskBoard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "../styles/TaskBoard.css";
+
+function TaskBoard() {
+  return (
+    <>
+      <div className="task-main">
+        <form className="task-wrapper">
+          <div className="task-inputs">
+            <div className="task-name">
+              <span>Task Name</span>
+              <input type="text" placeholder="Enter Task Name..." />
+            </div>
+            <div className="task-desc">
+              <span>Description</span>
+              <input type="text" placeholder="Enter Description..." />
+            </div>
+          </div>
+          <button className="add-task-button">Add Task</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default TaskBoard;
